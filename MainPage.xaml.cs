63dd2b2a212f13ac4319a25b17db5befdff31034
James Hines <ziplobob4@live.com>
2015-09-24 13:00:11 -0700

    Retry
    Hello :)

---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            inkCanvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen;
+        }
+
+        private void redButton_Click(object sender, RoutedEventArgs e)
+        {
+            InkDrawingAttributes inkDrawingAttributes = new InkDrawingAttributes();
+            inkDrawingAttributes.Color = Windows.UI.Colors.Red;
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(inkDrawingAttributes);
+        }
+
+        private void blueButton_Click(object sender, RoutedEventArgs e)
+        {
+            InkDrawingAttributes inkDrawingAttributes = new InkDrawingAttributes();
+            inkDrawingAttributes.Color = Windows.UI.Colors.Blue;
+            inkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(inkDrawingAttributes);
+        }
+    }
+}
+///fasfnaksjbfasijfbaisjbf
